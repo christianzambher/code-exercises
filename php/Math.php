fe2d73551d30f7cf343dd8d00c0b5ac95bfff4bc
Christian Emmanuel Zambrano Hernandez <christianzambher@gmail.com>
2026-05-03 23:17:59 -0600

    Codigo que explica la logica de las funciones matematicas

---
+++ php/Math.php
@@ -0,0 +1,19 @@
+<?php
+//Pi
+echo(pi()).PHP_EOL;
+//Min
+echo(min(0, 150, 30, 20, -8, -200)).PHP_EOL;
+//Max
+echo(max(0, 150, 30, 20, -8, -200)).PHP_EOL;
+//Valor absoluto
+echo(abs(-6.7)).PHP_EOL;
+//Raíz Cuadrada
+echo(sqrt(64)).PHP_EOL;
+//Redondeo
+echo(round(0.60)).PHP_EOL;
+echo(round(0.41)).PHP_EOL;
+//Aleatorio
+echo(rand()).PHP_EOL;
+//Aleatorio con rangos
+echo(rand(10, 100)).PHP_EOL;
+?>
